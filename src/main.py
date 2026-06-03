363534b01580e4b22a208c805922ab9d393c8e69
Mochatai <adnan.aljaroudi@gmail.com>
2026-06-03 10:13:39 -0400

    the project is done

--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ from blocksFunc import markdown_to_html_node
 import re
 import os
 import shutil
-
+import sys
 def clean_public():
-    path_to_clean = "public"
+    path_to_clean = "docs"
     content_to_delete = os.listdir(path_to_clean)
 
     for content in content_to_delete:
@@ -91,7 +91,7 @@ def one_to_one_copy(source_file, dis_file, ty):
 def clean_copy_public():
     clean_public()
     tes = get_all_content_paths("static")
-    copy_content(tes, "public")
+    copy_content(tes, "docs")
       
 
 
@@ -107,7 +107,7 @@ def extract_title(markdown: str):
 
     return markdown_strip_clean
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
 
     with open(from_path, 'r', encoding='utf-8') as from_path_file:
@@ -121,15 +121,18 @@ def generate_page(from_path, template_path, dest_path):
     page_title = extract_title(from_path__content)
     template_with_title = template_path_content.replace("{{ Title }}", str(page_title))
     template_with_title_content = template_with_title.replace("{{ Content }}", html_string)
+    template_with_title_content_href = template_with_title_content.replace(f'href="/"', f'href="{base_path}"')
+    template_with_title_content_href_src = template_with_title_content_href.replace(f'src="/"', f'src="{base_path}"')
+
 
     dest_path_clean = dest_path[:-2]
     dest_path_clean_html = dest_path_clean + "html"
 
     with open(dest_path_clean_html, "w", encoding="utf-8") as file:
-        file.write(template_with_title_content)
+        file.write(template_with_title_content_href_src)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     souce_content = get_all_content_paths(dir_path_content)
     dis_content = get_dis_files(souce_content,  dest_dir_path)
     #print("get_all_content", souce_content)
@@ -146,7 +149,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         elif souce_content[content] == "file":
 
             if f'{content[-2]}{content[-1]}' == "md":
-                generate_page(content, template_path, list(dis_content)[count])
+                generate_page(content, template_path, list(dis_content)[count], base_path)
                 count += 1
                 continue
 
@@ -155,8 +158,19 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             continue
 
 def main():
+
+    cli_input_len = len(sys.argv)
+
+    if (cli_input_len - 1) < 1:
+        basepath = "/"
+
+    else:
+        basepath = sys.argv[1]
+
+    
+
     clean_copy_public()
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 
 
